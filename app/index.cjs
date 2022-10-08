5d194c93bfd516355ea282869110f76c26039fbf
Zachary D. Skelton <zbert@hotmail.com>
2022-10-07 21:03:55 -0500

    Basic Functionality

--- app/index.cjs
+++ app/index.cjs
@@ -1,12 +1,32 @@
-console.log("hello world");
-
 const axious = require("axios");
 
+const enforceMax = (response) => {
+  let current = 0;
+  const max = 100;
+
+  try {
+    while (response.data.list[current].definition.length > max) {
+      current += 1;
+    }
+  } catch (_) {
+    current = 0;
+  }
+
+  return current;
+};
+
 async function getDefinition() {
-  const response = await axious.get(
-    "https://api.dictionaryapi.dev/api/v2/entries/en/hello"
-  );
-  console.log(response.data[0].meanings[0].definitions[0].definition);
+  try {
+    const response = await axious.get("https://api.urbandictionary.com/v0/random");
+    const num = enforceMax(response);
+
+    const word = response.data.list[num].word;
+    const definition = response.data.list[num].definition;
+    const site = response.data.list[num].permalink;
+    console.log(`${word}: ${definition}\n${site}`);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 getDefinition();
