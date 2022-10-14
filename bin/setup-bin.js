9ccd2b0fc9d1c6913babd44a650232cf46a57642
Zachary D. Skelton <zbert@hotmail.com>
2022-10-14 15:38:24 -0500

    Builds correctly.

---
+++ bin/setup-bin.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+/* eslint-disable */
+var join = require("path").join;
+var childProcess = require("child_process");
+var args = process.argv.slice(2);
+
+args.unshift(__dirname + "/../");
+
+childProcess.exec("npm start", (err, stdout) => {
+  if (err) console.log(err);
+  console.log(stdout);
+});
